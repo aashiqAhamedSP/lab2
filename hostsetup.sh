dcf2c35221ce15b93f9077c58ea5fd7351fee92f
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-11-28 16:15:50 +0000

    Update hostsetup.sh

--- hostsetup.sh
+++ hostsetup.sh
@@ -3,4 +3,4 @@ mysqlvmpubip=$(az vm show -d -g lab2rg -n mysqlvm --query publicIps -o tsv)
 touch inventory
 echo "[remotehosts]" > ./inventory
 echo "$mysqlvmpubip ansible_connection=ssh" >> ./inventory
-rm ~/.ssh/known_hosts
+#rm ~/.ssh/known_hosts
